6e341ccf2752c45ddc317a96edc7cd3901dd4b8b
ericstephan <ericphb@gmail.com>
2019-10-15 15:43:33 -0700

    removed the tags and fixed the epoch time to be based on UTC

--- timeseries/loadprofiles/loadprofile_measurement_bulk.py
+++ timeseries/loadprofiles/loadprofile_measurement_bulk.py
@@ -11,10 +11,10 @@ raw_input_file = "ieeezipload.player"
 measurement = "loadprofile"
 bulk_load_output_file = "loadprofile_measurement_out.txt"
 database = "proven"
-month = 1
-day = 1
-year = 2018
 
+#Jan/1/2018 00:00:00
+seed_epoch_date = 1514764800
+epoch_increment = 60
 
 def create_import_header_lines(database):
     s = "CREATE DATABASE " + database + "\n"
@@ -32,106 +32,21 @@ def strip_extra_chars(s):
         return s
 
 
-
-def pad_number (number):
-    num_str = ""
-    if (number < 10):
-        num_str = "0" + str(number)
-    else:
-        num_str = str(number)
-        
-    return num_str;
-
-def next_time(hour, minute):
-    next_minute = minute
-    next_hour = hour
-    if (minute == 59):
-        next_minute = 0;
-        next_hour = hour + 1
-    else:
-        next_minute = minute + 1
-    if (hour == 23):
-        if (minute == 59):
-            next_hour = 0
-            next_minute = 0
-        else:
-            next_minute = minute + 1
-    return next_hour, next_minute
-
-def next_date(year, month, day):
-    next_day = day
-    next_month = month
-    next_year = year
-    if (year % 400 == 0):
-        leap_year = True
-    elif (year % 100 == 0):
-        leap_year = False
-    elif (year % 4 == 0):
-        leap_year = True
-    else:
-        leap_year = False
-    
-    
-    if month in (1, 3, 5, 7, 8, 10, 12):
-        month_length = 31
-    elif month == 2:
-        if leap_year:
-            month_length = 29
-        else:
-            month_length = 28
-    else:
-        month_length = 30
-    
-    
-    if day < month_length:
-        next_day += 1
-    else:
-        next_day = 1
-        if month == 12:
-            next_month = 1
-            next_year += 1
-        else:
-            next_month += 1
-    return next_month, next_day
-
-def process_day(month,day,year):
-    hour = 0
-    minute = 0
-    timezone = ""
-    flag =  True
-    # fw = open("loadplayer.txt")
-    with open(raw_input_file) as fp:
-       line = fp.readline()
-       line_tokens = line.split(",")
-       dt_tokens = line_tokens[0].split(" ")
-       if (len(dt_tokens) == 3): 
-           start_date_str = dt_tokens[0]
-           start_time_str = dt_tokens[1]
-           timezone = dt_tokens[2]
-           newline = strip_extra_chars(line_tokens[1])  
-
-       while line:
-    #       print("Line {}: {}".format(cnt, line.strip()))
-           line = fp.readline()
-           newline = strip_extra_chars(line)
-           tokens = newline.split(",")
-           if len(tokens) == 2 :
-            hour,minute = next_time(hour, minute)
-#            date_time = '29.08.2011 11:05:02'
-            date_time = pad_number(day) + "." + pad_number(month) + "." + str(year) + " " + pad_number(hour) + ":" +  pad_number(minute) + ":00"
-            pattern = '%d.%m.%Y %H:%M:%S'
-            epoch = int(time.mktime(time.strptime(date_time, pattern)))
-            fo.write(measurement + "," + "DATE=" + "'" + pad_number(day) +  "/" +  pad_number(month) + "/" + str(year) + "'," + "UTC='" + pad_number(hour) + ":" +  pad_number(minute) + "' value=" + tokens[1] + " " + str(epoch) + "\n") 
-         
-         
-   
 bulkload_file = bulk_load_output_file
 fo = open(bulkload_file,'w')
 fo.write(create_import_header_lines(database) + "\n")
-for day_counter in range(1,366):
-    process_day(month,day, year)
-    month, day = next_date(2018, month, day)
-    day_counter = day_counter + 1
 
+epoch_index = seed_epoch_date
+
+for day in range(1,366):
+     with open(raw_input_file) as fp:
+          line = fp.readline()
+          while line:
+               newline = strip_extra_chars(line)
+               tokens = newline.split(",")
+               if len(tokens) == 2 :
+                    fo.write(measurement + " value=" + tokens[1] + " " + str(epoch_index) + "\n") 
+               epoch_index = epoch_index + epoch_increment
+               line = fp.readline()
 fo.close()
 print ("created..." + bulk_load_output_file)
